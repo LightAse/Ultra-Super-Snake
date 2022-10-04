5948d936ab160582218de49e5fbbc8d1016823f0
LightAse <ianfridman9@gmail.com>
2022-10-04 17:00:02 -0300

    base

--- controllers/user.js
+++ controllers/user.js
@@ -1,26 +1,37 @@
 require('mongoose');
 const Usr = require('../models/user');
 
-const addUser = async (name,lastname,email) => {
 
-    
-    let existUser = await Usr.findOne({email: email});
+const addUser = async (name,lastname,email,isActive,password) => {
+
+    let existUser = await Usr.findOne({ email: email });
     console.log(existUser);
-    if(!existUser){
+    if(!existUser) {
+
+        const cryptoPass = require('crypto')
+        .createHash('sha256')
+        .update(password)
+        .digest('hex');
+        
         const usr = new Usr(
-            {
-                name:name,
+            {              
+                name: name,
                 lastname:lastname,
-                email:email
+                email: email,
+                isActive:isActive,
+                password:cryptoPass
             }
-        )
+        );
+
+        let user = await usr.save(); 
+        console.log("usuario nuevo");
+        console.log(user);
+        return { user }; 
+
     }else{
         return false;
     }
-
-
-}
-
+}   
 
 const getAllUsers = async (limit,offset) => {
 
@@ -33,18 +44,30 @@ const getUser = async(id) => {
 
     const user = await Usr.findById(id);
 
-    return user;
+    // await Usr.findOne({ _id: req.params.id })
 
+    return user;
 }
 
 const editUser = async(user) => {
 
     const result = await Usr.findByIdAndUpdate(user._id,user,{new:true});
-    
-    return result;
 
+    return result;
 }
 
+const editRoles = async(roles,id) => {
 
+    const result = await Usr.findByIdAndUpdate(id,{$set:{roles:roles}},{new:true});
 
-module.exports = {addUser, getAllUsers, getUser, editUser}
+    return result;
+}
+
+const deleteUser = async(id) => {
+
+    const result = await Usr.findByIdAndDelete(id);
+
+    return result;
+}
+
+module.exports = { addUser, getAllUsers, getUser, editUser, editRoles, deleteUser }
--- index.js
+++ index.js
@@ -7,10 +7,10 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGO_URI;
 
-
-/********/
+/******/
 const UsrController = require('./controllers/user');
-const Usr = require("./models/user");
+const AuthController = require('./controllers/auth');
+const Middleware = require('./middleware/auth-middleware');
 
 
 mongoose
@@ -36,38 +36,35 @@ app.post("/",(req,res) => {
 });
 
 // Get de todos los usuarios
-app.get("/users",(req,res) =>{
-    
-    let limit = req.query.limit;
-    let offset = req.query.offset;
+app.get("/users",Middleware.verify,async (req,res) =>{
 
-    try{
-        const results = UsrController.getAllUsers(limit,offset);
-        res.status(200).json(results);
+  let limit = req.query.limit;
+  let offset = req.query.offset;
 
-    }catch(error){
+  try{
+      const results = await UsrController.getAllUsers(limit,offset);
+      res.status(200).json(results);
 
-        res.status(500).send("Error. intente mas tarde");
+  }catch(error){
+      res.status(500).send("Error. Intente más tarde.")
+  }
 
-    }
-
-    res.send("Get Users");
 });
 
 // Get Info de un usuario
 
-app.get("/users/:id",(req,res) =>{
+app.get("/users/:id",async (req,res) =>{
 
     let userId =  req.params.id;
 
-    try {
-        
-        user =  await UsrController.getUser(userId);
+    try{
 
-        res.status(200).json(user);
+      user = await UsrController.getUser(userId);
 
-    } catch (error) {
-        res.status(500).send("Error 500");
+      res.status(200).json(user);
+
+    }catch(error){
+      res.status(500).send("Error");
     }
 
 });
@@ -75,44 +72,94 @@ app.get("/users/:id",(req,res) =>{
 // Creo un nuevo usuario
 
 app.post("/users",async (req,res) =>{
-
+    
     let name = req.body.name;
     let lastname = req.body.lastname;
     let email = req.body.email;
+    let isActive = req.body.isActive;
+    let password = req.body.password;
     try{
-        const result = await UsrController.addUser(name,lastname,email);
-        if(result){
-            res.status(201).send("Usuario creado correctamenta"); // 201
-        }else{
-            res.status(409).send('El usuario ya existe'); // 409
-        }
+      const result = await UsrController.addUser(name,lastname,email,isActive,password);
+      if(result){
+        res.status(201).send("Usuario creado correctamente"); // 201
+      }else{
+        res.status(409).send("El usuario ya existe"); // 409
+      }  
     }catch(error){
-        res.status(500).send("Error al crear el usuario."); //500
-    }
+      res.status(500).send("Error al crear el usuario."); //500
+    }  
+    
 });
 
 // Modifico un usuario
 app.put("/users/:id",async (req,res) =>{
 
-    const user= {_id:req.params, ...req.body};
-    try {
-    
-        const result = await UsrController.editUser(user);
+    const user = { _id: req.params.id, ...req.body };
+    //             {_id: req.params.id, name: req.body.name, lastname, email }
+    try{
+      
+      const result = await UsrController.editUser(user);
+      if(result){
+        res.status(200).json(result);
+      }else{
+        res.status(404).send("El usuario no existe.");
+      }  
+    }catch(error){  
+       res.status(500).send("Error");
+    } 
 
-    } catch (error) {
-        
-    }
-    
-
-
-
-    res.send("Modifico un usuario");
 });
 
 // Elimino un usuario
-app.delete("/users/:id",(req,res) =>{
-    res.send("Elimmino un usuario");
+app.delete("/users/:id", async(req,res) =>{
+
+    try{
+
+      const result = await UsrController.deleteUser(req.params.id);
+      if(result){
+        res.status(200).send("Usuario borrado.")
+      }else{
+        res.status(404).send("No se ha podido eliminar el usuario.")
+      }  
+
+    }catch(error){
+      res.status(500).send("Error")
+    }
 });
+
+app.put("/users/:id/roles",async (req,res) =>{
+    
+    const roles = req.body.roles;
+    //const user = { _id: req.params.id, ...req.body };
+    try{
+      
+      const result = await UsrController.editRoles(roles,req.params.id);
+      if(result){
+        res.status(200).json(result);
+      }else{
+        res.status(404).send("El usuario no existe.");
+      }  
+    }catch(error){  
+       res.status(500).send("Error");
+    } 
+})
+
+app.post("/auth/login", async (req,res) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+    try{
+      const result = await AuthController.login(email,password);
+      if(result){
+        res.status(200).json(result);
+      }else{
+        res.status(401).send("No puede estar aqui")
+      }
+    }catch(error){
+        res.status(500).send("Error");
+    }  
+})
+
 
 http.listen(PORT, () => {
   console.log(`Listening to ${PORT}`);
